eab673e68456aa1267c66bc57181440c19e34c04
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-12 16:33:56 +0300

    change layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,16 +2,17 @@ import { Outlet } from "react-router";
 
 export const AppLayout = () => {
   return (
-    <>
-      <header>
-        <h1>Header</h1>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <h4>Footer</h4>
-      </footer>
-    </>
+    <div className="flex h-screen">
+      <div className="flex flex-col w-16 md:w-60 p-6">Sidebar</div>
+
+      <div className="flex flex-col items-center flex-1 overflow-hidden">
+        <header className="h-16 flex items-center px-6 mb-10">
+          <h1 className="text-xl font-semibold">Main</h1>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
--- src/components/ui/TestComponent.tsx
+++ src/components/ui/TestComponent.tsx
@@ -1,9 +1,9 @@
 const TestComponent = () => {
-    return (
-<div className="bg-white/30 backdrop-blur-xs border border-white/40 rounded-xl p-7">
-  Glass content
-</div>
-    )
-}
+  return (
+    <div className="bg-white/30 backdrop-blur-xs border border-white/40 rounded-xl p-7">
+      Glass content
+    </div>
+  );
+};
 
 export default TestComponent;
