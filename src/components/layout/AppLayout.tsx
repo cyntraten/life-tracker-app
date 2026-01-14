777255ea9930ded31fdafe5cdaabf82a383a0efc
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-14 17:17:29 +0300

    change styles in AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,11 +6,11 @@ export const AppLayout = () => {
     <div className="flex h-screen">
       <Sidebar />
 
-      <div className="flex flex-col items-center flex-1 overflow-hidden mr-2 ">
+      <div className="flex flex-col items-center flex-1 overflow-hidden mr-2 w-full">
         <header className="h-16 flex items-center px-6 mb-10">
           <h1 className="text-xl font-semibold">Main</h1>
         </header>
-        <main>
+        <main className="w-full">
           <Outlet />
         </main>
       </div>
