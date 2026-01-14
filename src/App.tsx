4b068590388b709756b2f0f2ae90d7cd6e9f46f9
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-14 16:34:25 +0300

    move router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import TestComponent from "./components/ui/TestComponent";
+import { Route, Routes } from "react-router";
+import DashboardPage from "./components/DashboardPage";
+import { AppLayout } from "./components/layout/AppLayout";
 
 function App() {
   return (
-    <div>
-      <TestComponent />
-    </div>
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/habits" element={<DashboardPage />} />
+        <Route path="/tasks" element={<DashboardPage />} />
+        <Route path="/mood" element={<DashboardPage />} />
+        <Route path="/journal" element={<DashboardPage />} />
+        <Route path="/stats" element={<DashboardPage />} />
+      </Route>
+    </Routes>
   );
 }
 
