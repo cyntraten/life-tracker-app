2d4535851d057033533a28fedf9b8ba5f68cee2f
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-15 14:26:48 +0300

    remove header from AppLayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,12 +5,8 @@ export const AppLayout = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-
       <div className="flex flex-col items-center flex-1 overflow-hidden mr-2 w-full">
-        <header className="h-16 flex items-center px-6 mb-10">
-          <h1 className="text-xl font-semibold">Main</h1>
-        </header>
-        <main className="w-full">
+        <main className="w-full mt-2">
           <Outlet />
         </main>
       </div>
