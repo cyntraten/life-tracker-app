b3a7a1ef12abd31719cda7b798f4288975553b24
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-22 16:19:18 +0300

    feat(store): update habit actions in store

--- src/store/useLifeStore.ts
+++ src/store/useLifeStore.ts
@@ -11,7 +11,8 @@ interface Habit {
   id: string;
   name: string;
   streak: number;
-  doneToday: boolean;
+  lastCompleted: number;
+  progressPercent: number;
 }
 
 type State = {
@@ -23,11 +24,31 @@ type Action = {
   addTask: (task: Task) => void;
   addHabit: (habit: Habit) => void;
   toggleTask: (id: string) => void;
+  doneHabit: (id: string, timestamp: number) => void;
+  cancelHabit: (id: string) => void;
 };
 
 const useLifeStore = create<State & Action>()((set) => ({
-  tasks: [],
-  habits: [],
+  tasks: [
+    { id: "t1", title: "Задача 1", done: false, date: "1769078858742" },
+    { id: "t2", title: "Задача 2", done: false, date: "1669078858742" },
+  ],
+  habits: [
+    {
+      id: "h1+555",
+      name: "Привычка 1",
+      streak: 22,
+      lastCompleted: 1769078858742,
+      progressPercent: 33,
+    },
+    {
+      id: "h2",
+      name: "Привычка 2",
+      streak: 0,
+      lastCompleted: 1669078858742,
+      progressPercent: 0,
+    },
+  ],
   addTask: (task: Task) => set((state) => ({ tasks: [...state.tasks, task] })),
   toggleTask: (id: string) =>
     set((state) => ({
@@ -37,6 +58,35 @@ const useLifeStore = create<State & Action>()((set) => ({
     })),
   addHabit: (habit: Habit) =>
     set((state) => ({ habits: [...state.habits, habit] })),
+  doneHabit: (id: string, timestamp: number) =>
+    set((state) => ({
+      habits: state.habits.map((habit) =>
+        habit.id === id
+          ? {
+              ...habit,
+              streak: habit.streak + 1,
+              lastCompleted: timestamp,
+              progressPercent: Math.floor((habit.streak + 1) / 0.66),
+            }
+          : habit,
+      ),
+    })),
+  cancelHabit: (id: string) =>
+    set((state) => ({
+      habits: state.habits.map((habit) =>
+        habit.id === id
+          ? {
+              ...habit,
+              streak: Math.max(habit.streak - 1, 0),
+              lastCompleted: 0,
+              progressPercent: Math.max(
+                Math.floor((habit.streak - 1) / 0.66),
+                0,
+              ),
+            }
+          : habit,
+      ),
+    })),
 }));
 
 export default useLifeStore;
