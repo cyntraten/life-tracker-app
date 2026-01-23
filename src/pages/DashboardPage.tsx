b438ecb1fdcbb0831e6cfd5262d7f2689d080b98
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-23 12:08:55 +0300

    feat(dashboard): add UpcomingEvents to dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,6 +2,7 @@ import DashboardHello from "../components/dashboard/DashboardHello";
 import HabitsProgress from "../components/dashboard/HabitsProgress";
 import QuickAddTask from "../components/dashboard/QuickAddTask";
 import TodayTasks from "../components/dashboard/TodayTasks";
+import UpcomingEvents from "../components/dashboard/UpcomingEvents";
 import { GlassCard } from "../components/ui/GlassCard";
 
 export default function DashboardPage() {
@@ -14,6 +15,7 @@ export default function DashboardPage() {
         <QuickAddTask />
         <TodayTasks />
         <HabitsProgress />
+        <UpcomingEvents />
       </GlassCard>
     </div>
   );
