4522d97f400844bb05c8cf42cab048a71808a207
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-25 10:37:40 +0300

    feat(layout): add overflow to layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,7 +5,7 @@ export const AppLayout = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex flex-col items-center flex-1 overflow-hidden mr-2 w-full">
+      <div className="flex flex-col items-center flex-1 overflow-auto mr-2 w-full">
         <main className="w-full mt-2">
           <Outlet />
         </main>
