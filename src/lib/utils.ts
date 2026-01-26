9626074e02f4e5a914bd8eeefd95465841bd59aa
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-26 17:06:34 +0300

    update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,5 +31,11 @@ export function formatEventDate(timestamp: number): string {
     return "dayAfter";
   }
 
-  return "soon";
+  const date = new Date(timestamp);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
 }
