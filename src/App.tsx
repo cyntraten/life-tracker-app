9abe06278a0e88b24c59a0b8aa9ed309ea2fbcf2
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-01-27 23:38:55 +0300

    update routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router";
 import DashboardPage from "./pages/DashboardPage";
 import { AppLayout } from "./components/layout/AppLayout";
 import TaskPage from "./pages/TaskPage";
+import HabitsPage from "./pages/HabitsPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<DashboardPage />} />
-        <Route path="/habits" element={<DashboardPage />} />
+        <Route path="/habits" element={<HabitsPage />} />
         <Route path="/tasks" element={<TaskPage />} />
         <Route path="/mood" element={<DashboardPage />} />
         <Route path="/journal" element={<DashboardPage />} />
