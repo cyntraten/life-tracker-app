1d5009e816e6e7de1ac7e6479894a8e00ac35bc3
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-04-07 18:17:38 +0300

    fix(stats): load all data on app startup

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,21 @@
 import { Outlet } from "react-router";
 import Sidebar from "./Sidebar";
+import { useEffect } from "react";
+import useTaskStore from "../../store/useTaskStore";
+import useHabitStore from "../../store/useHabitStore";
+import useMoodStore from "../../store/useMoodStore";
 
 export const AppLayout = () => {
+  const { loadTasksFromDB } = useTaskStore();
+  const { loadHabitsFromDB } = useHabitStore();
+  const { loadMoodsFromDB } = useMoodStore();
+
+  useEffect(() => {
+    loadTasksFromDB();
+    loadHabitsFromDB();
+    loadMoodsFromDB();
+  }, [loadTasksFromDB, loadHabitsFromDB, loadMoodsFromDB]);
+
   return (
     <div className="flex h-screen">
       <Sidebar />
