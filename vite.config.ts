a0ca9f3e13331568691e0273aa9b434ecbc3c8f2
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-04-09 20:25:30 +0300

    feat(vite): load env variables using loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8080",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  if (!env.VITE_BACKEND_URL) {
+    throw new Error(
+      "VITE_BACKEND_URL is not defined in the environment variables",
+    );
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": env.VITE_BACKEND_URL,
+      },
     },
-  },
+  };
 });
